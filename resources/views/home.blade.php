64a77f51ec47284d4d55fad0274611f9d65f0010
akoper <andrew.koper@gmail.com>
2019-01-06 15:43:58 -0500

    Modified home screen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <div class="card-header">Home/vestigial screen</div>
 
-                    You are logged in!
-                </div>
+    <div class="card-body">
+        @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
             </div>
-        </div>
+        @endif
+
+        This is a vestigial screen with no meaning.  If you are seeing it, something is probably wrong.
     </div>
-</div>
+
 @endsection
