837c738a8fe416968a2929d005a238dda594525e
andrew_koper <andrew.koper@gmail.com>
2019-01-18 19:22:12 -0500

    Create user and then they go to their homepage

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/users';
+    protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
@@ -74,6 +74,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+
         return User::create([
             'first_name' => $data['first_name'],
             'last_name' => $data['last_name'],
